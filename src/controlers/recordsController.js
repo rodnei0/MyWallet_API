a6f7af69d63e20a2d6222a9ce95193772c2a9f73
rodnei0 <rodnei0@gmail.com>
2022-02-04 16:23:41 -0400

    requirements implemented

--- src/controlers/recordsController.js
+++ src/controlers/recordsController.js
@@ -1,11 +1,12 @@
 import dayjs from "dayjs";
 import ptbr from 'dayjs/locale/pt-br.js';
 import db from "../db.js";
+
 export async function insertRecord (req, res) {
     try {
         const entryRecord = req.body;
         const user = res.locals.user;
-        
+
         const recordsCollection = db.collection(process.env.MONGO_RECORDS);
         const now = dayjs().locale(ptbr).format("DD/MM");
         await recordsCollection.insertOne({...entryRecord, userId: user._id ,date: now});
@@ -17,17 +18,16 @@ export async function insertRecord (req, res) {
     }
 };
 
-// export async function getRecord (req, res) {
+export async function getRecord (req, res) {
+    try{
+        const user = res.locals.user;
 
-
-//     try{
-//         const recordsCollection = db.collection(process.env.MONGO_RECORDS);
-//         const now = dayjs().locale(ptbr).format("DD/MM");
-//         await recordsCollection.insertOne({...entryRecord, date: now});
+        const recordsCollection = db.collection(process.env.MONGO_RECORDS);
+        const records = await recordsCollection.find({userId: user._id}).toArray();
         
-// 		res.sendStatus(201);
-//     } catch (error) {
-//         res.status(500).send('A culpa foi do estagiário');
-//         console.log(error);
-//     }
-// };
+		res.status(201).send(records);
+    } catch (error) {
+        res.status(500).send('A culpa foi do estagiário');
+        console.log(error);
+    }
+};
--- src/routes/recordsRouter.js
+++ src/routes/recordsRouter.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { insertRecord } from "../controlers/recordsController.js";
+import { getRecord, insertRecord } from "../controlers/recordsController.js";
 import validateRecordsSchema from "../midllewares/validateRecordsSchema.js";
 import { validateToken } from "../midllewares/validateToken.js";
 
@@ -7,5 +7,6 @@ const recordsRouter = Router();
 
 recordsRouter.use(validateToken);
 recordsRouter.post("/records", validateRecordsSchema, insertRecord);
+recordsRouter.get("/records", getRecord);
 
 export default recordsRouter;
